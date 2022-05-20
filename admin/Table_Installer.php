c1436f9068e3d83cb14b90567ad13fb1e5cc254c
Ashique <ashique12009@gmail.com>
2022-05-20 18:01:20 +0600

    slider table created during plugin activation

---
+++ admin/Table_Installer.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * Table Installer Class
+ *
+ */
+class Table_Installer {
+
+    /**
+     * Binding all events
+     *
+     * @since 1.0
+     *
+     * @return void
+     */
+    public function __construct() {
+        $this->activate();
+    }
+
+    /**
+     * Placeholder for activation function
+     * Nothing being called here yet.
+     *
+     * @since 1.0
+     *
+     * @return void
+     */
+    public function activate() {
+        update_option('ashique_bs4_slider_version', ASHIQUE_BS4_SLIDER_VERSION);
+        $this->create_slider_table();
+    }
+
+    /**
+     * Create necessary table for ERP & HRM
+     *
+     * @since 1.0
+     *
+     * @return  void
+     */
+    public function create_slider_table() {
+        global $wpdb;
+
+        $collate = '';
+
+        if ($wpdb->has_cap('collation')) {
+            if (!empty($wpdb->charset)) {
+                $collate .= "DEFAULT CHARACTER SET $wpdb->charset";
+            }
+
+            if (!empty($wpdb->collate)) {
+                $collate .= " COLLATE $wpdb->collate";
+            }
+        }
+
+        $table_schema = [
+            "CREATE TABLE IF NOT EXISTS `{$wpdb->prefix}ashique_bs4_slider` (
+                `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+                `title` VARCHAR(128) NOT NULL,
+                `image` VARCHAR(255) NOT NULL,
+                `link` VARCHAR(255) NOT NULL,
+                `status` BOOLEAN NOT NULL DEFAULT '1',
+                `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                PRIMARY KEY (`id`)
+            ) $collate;,"
+        ];
+
+        require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+        foreach ($table_schema as $table) {
+            dbDelta($table);
+        }
+    }
+}
--- ashique-bs4-slider.php
+++ ashique-bs4-slider.php
@@ -11,14 +11,26 @@
  * Domain Path:       /languages
  */
 
+// If this file is called directly, abort.
+if (! defined('ABSPATH')) {
+	die('Invalid request.');
+}
+
 /**
  * The main plugin class
  */
 final class Bs4_Slider
 {
+
+    const version = '1.0.0';
+
     public function __construct()
     {
+        $this->define_constants();
+
+        register_activation_hook(__FILE__, [$this, 'activate']);
         
+        add_action('plugins_loaded', [$this, 'initialize_plugin']);
     }
 
     /**
@@ -33,6 +45,32 @@ final class Bs4_Slider
         }
 
         return $instance;
+    }
+
+    /**
+     * Define all necessary constants
+     */
+    public function define_constants()
+    {
+        define('ASHIQUE_BS4_SLIDER_VERSION', self::version);
+        define('ASHIQUE_BS4_SLIDER_FILE', __FILE__);
+        define('ASHIQUE_BS4_SLIDER_PATH', __DIR__);
+        define('ASHIQUE_BS4_SLIDER_URL', plugins_url('', ASHIQUE_BS4_SLIDER_FILE));
+        define('ASHIQUE_BS4_SLIDER_ASSETS', ASHIQUE_BS4_SLIDER_URL . '/assets');
+    }
+
+    /**
+     * Create slider table during activation of plugin
+     */
+    public function activate()
+    {
+        require_once ASHIQUE_BS4_SLIDER_PATH . '/admin/Table_Installer.php';
+        new Table_Installer();
+    }
+
+    public function initialize_plugin()
+    {
+        
     }
 }
 
