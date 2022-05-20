de9a3adfff6b0448588c612517e931fe44d5e4ec
Ashique <ashique12009@gmail.com>
2022-05-20 17:19:40 +0600

    initial commit

---
+++ ashique-bs4-slider.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name:       Bootstrap 4 Carousel Slider
+ * Plugin URI:        https://ashique12009.blogspot.com/
+ * Description:       A simple bootstrap 4 carousel slider.
+ * Version:           1.0.0
+ * Author:            Khandoker Ashique Mahamud
+ * Author URI:        https://ashique12009.blogspot.com/
+ * License:           GPL v2 or later
+ * Text Domain:       ashique-bs4-slider
+ * Domain Path:       /languages
+ */
+
+/**
+ * The main plugin class
+ */
+final class Bs4_Slider
+{
+    public function __construct()
+    {
+        
+    }
+}
